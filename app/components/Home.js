be52a396ebe307abef7a4a088e075519a4ac3b3b
Nenjo Tsu <renenjo.valente@gmail.com>
2018-02-17 09:30:23 +0800

    add antdesing

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import DatePicker from 'antd/lib/date-picker';
 import styles from './Home.css';
 
 type Props = {};
@@ -13,6 +14,8 @@ export default class Home extends Component<Props> {
       <div>
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
+          <DatePicker />
+          <br />
           <Link to="/counter">to Counter</Link>
         </div>
       </div>
